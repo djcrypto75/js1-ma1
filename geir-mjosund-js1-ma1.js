900d5ed44f98cef73f377b50e7773ae08e2dfb01
dj-crypto <33126138+dj-crypto@users.noreply.github.com>
2020-01-26 16:08:10 +0100

    first attempt
    first attempt

---
+++ geir-mjosund-js1-ma1.js
@@ -0,0 +1,74 @@
+// question 1
+const cat = {
+   name: "Pusur",
+   age: 12,
+   dangerous: true,
+
+   // bark is a property with a function as its value
+   complain: function() {
+      console.log("Meow");
+   }
+};
+cat.complain();
+
+// question 2
+const heading = document.querySelector("h3");
+
+// question 3
+const heading = document.querySelector("h3");
+heading.style.fontSize = "2em";
+
+// question 4
+const heading = document.querySelector("h3");
+heading.classList.add("subheading");
+
+// question 5
+const paragraphs = document.querySelectorAll("p");
+
+// question 6
+const resultsContainer = document.querySelector("div");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+
+// question 7
+function catNames(catArray) {
+   for (let i = 0; i < catArray.length; i++) {
+      console.log(catArray[i].name);
+   }
+}
+catNames(cats);
+
+// question 8
+function catNames(catArray) {
+   let catHTML = "";
+   for (let i = 0; i < catArray.length; i++) {
+      catHTML += `<h5>${catArray[i].name}</h5>`;
+   }
+   return catHTML;
+}
+
+// question 9
+
+// question 10
+const container = document.querySelector(".container");
+let catHTML = "";
+
+for (let i = 0; i < cats.length; i++) {
+   let catAge = "Age unknown";
+   if (cats[i].age) {
+      catAge = cats[i].age;
+   }
+   const catDetails = `<div>
+                    <h5>${cats[i].name}</h5>
+                    <p>${catAge}</p>
+                    </div>`;
+   catHTML += catDetails;
+}
+container.innerHTML = catHTML;
+
+function catNames(catArray) {
+   let catHTML = "";
+   for (let i = 0; i < catArray.length; i++) {
+      catHTML += `<h5>${catArray[i].name}</h5>`;
+   }
+   return catHTML;
+}
